a158387dcbf0f47527408ef145c82420d2906b68
BuildTools <unconfigured@null.spigotmc.org>
2026-05-14 23:30:37 +0200

    Improved employee name validation

--- DemoKROS/Constants/ValidationPatterns.cs
+++ DemoKROS/Constants/ValidationPatterns.cs
@@ -2,7 +2,7 @@
 
 public static class ValidationPatterns
 {
-    public const string PersonName = @"^[\p{L}\p{M}'\- ]+$";
+    public const string PersonName = @"^\p{Lu}[\p{L}\p{M}'\- ]*$";
     public const string OrganizationName = @"^[\p{L}\p{N}\p{M}&().,\-'/ ]+$";
     public const string OrganizationCode = @"^[A-Z0-9\-_]+$";
 }
